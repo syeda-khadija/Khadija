2162722857a8de81ff2272c4a68dfcb78474e117
Syeda_Khadija <khadijasyeda128@gmail.com>
2025-06-24 13:01:06 +0500

    commit

--- METHODS.R
+++ METHODS.R
@@ -186,22 +186,70 @@
 # )
 # print(movie_info)
 
-#Employee
 
+
+#Employee
 Name <- c("Sara","Ali","Ahmed","Amal","Sameer")
 no_of_workingdays <- c(28,19,18,8,10)
 no_of_lateArrival <-c(4,6,2,1,0)
 Department  <-c("Hr","IT","Marketing","Accounting","Product Management")
 Designation <- c("Assistant","Employee","Employee","Manager","CEO")
 Gender <-c("Female","Male","Male","Female","Male")
-Is_permenant <-c("True","True","False","True","True")
+Is_permenant <-c(TRUE,TRUE,FALSE,TRUE,TRUE)
 
 Employee_info <-data.frame(
   Name =Name,
   Late_arrival=no_of_lateArrival,
-  Department =Department,
-  Designation=Designation,
-  Gender=Gender,
-  Is_permenant =Is_permenant
+  department =Department,
+  designation=Designation,
+  gender=Gender,
+  is_permenant=Is_permenant
 )
 print(Employee_info)
+#Display all employee names and their departments.
+print(Employee_info[,c(1,4)])
+
+#Show only permanent employees. 
+print(Employee_info[Employee_info$is_permenant == TRUE,])
+
+#Filter employees from the IT department.
+print(Employee_info[Employee_info$department == "IT",])
+
+#Find employees with more than 2 late arrivals.
+print(Employee_info[Employee_info$Late_arrival <2,])
+
+#Display names and designations of employees who are not permanent.
+print(Employee_info[Employee_info$is_permenant == FALSE,c(1,5)])
+
+#List of employees who had zero late arrivals.
+print(Employee_info[Employee_info$Late_arrival == 0,])
+
+#Show records from rows 2 to 5.
+print(Employee_info[2:5,])
+
+#Display names of employees at row positions 1,3, and 4.
+print(Employee_info[c(1,3,4),1])
+
+#Display only the Name and Department of employees from rows 1 to 3.
+print(Employee_info[1:3,c(1,3)])
+
+#Access the designation of the employee at row 4
+print(Employee_info[4,4])
+
+#Access the first 3 rows using head().
+print(head(Employee_info,n=3))
+
+#Access the last 2 rows using tail().
+print(tail(Employee_info,n=2))
+
+#Get the gender of the employee in row 2.
+print(Employee_info[2,5])
+
+#Show the 1st employee record.
+print(Employee_info[1,])
+
+#Add a new column called City showing the city where each employee is based.  
+Employee_info$City <-c("karachi","lahore","karachi","Islamabad","multan")
+
+#Add a new column Joining_Year to show the year each employee joined.
+Employee_info$joining_year <-c(2019,2017,2023,2025,2023)
